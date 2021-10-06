0a248338df4c51bdb5bc3ab93216cb7c7c651710
Muammer Sarı <sr_muammer@hotmail.com>
2021-10-06 05:20:31 +0300

    06100519
    Mssql server için kütüphaneler yüklendi

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace ToDoList.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Home()
         {
             return View();
         }
